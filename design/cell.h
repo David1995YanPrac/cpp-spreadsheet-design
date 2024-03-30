54f6392f8215280ce4fa06c1527545cc953ba70e
David1995YanPrac <130498738+David1995YanPrac@users.noreply.github.com>
2024-03-30 18:03:07 +0500

    Dobriy den) Sprint 15 дизайн

--- design/cell.h
+++ design/cell.h
@@ -3,9 +3,15 @@
 #include "common.h"
 #include "formula.h"
 
+#include <functional>
+#include <unordered_set>
+#include <optional>
+
+class Sheet;
+
 class Cell : public CellInterface {
 public:
-    Cell();
+    Cell(Sheet& sheet);
     ~Cell();
 
     void Set(std::string text);
@@ -13,11 +19,20 @@ public:
 
     Value GetValue() const override;
     std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
 
 private:
     class Impl;
     class EmptyImpl;
     class TextImpl;
     class FormulaImpl;
+
+    bool CheckForCyclicity(const Impl& impl) const;
+    void DoInvalidCache(bool check_changes = false);
+   
     std::unique_ptr<Impl> impl_;
+
+    Sheet& sheet_;
+    std::unordered_set<Cell*> left_nods_;
+    std::unordered_set<Cell*> right_nods_;
 };
